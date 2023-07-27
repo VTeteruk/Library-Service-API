ae5a60ec0b45d7121c35bb7dbf05e6c509ad15ea
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-27 15:28:30 +0300

    Implement Book model & make migrations

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverTypeChoices(models.TextChoices):
+        HARD = "H", "Hard"
+        SOFT = "S", "Soft"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=10, choices=CoverTypeChoices.choices)
+    inventory = models.IntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.author})"
