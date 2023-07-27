c47527ba231e067e5c03f766d392df76bc9e16bf
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-27 17:07:05 +0300

    Add IsAuthenticated permission to `me` endpoint

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from users.models import User
 from users.serializers import UserSerializer
@@ -8,6 +9,7 @@ from users.serializers import UserSerializer
 class UserView(generics.RetrieveUpdateDestroyAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self) -> User:
         return get_user_model().objects.get(id=self.request.user.id)
