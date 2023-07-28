b51f3dfd4a1041fea3e62bbde0939e442eeb1396
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-28 12:34:48 +0300

    Add swagger documentation

--- library_service_api/urls.py
+++ library_service_api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -24,5 +25,11 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("users.urls", namespace="users")),
-    path("api/", include("borrowings.urls", namespace="borrowings"))
+    path("api/", include("borrowings.urls", namespace="borrowings")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger",
+    ),
 ]
