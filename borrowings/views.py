5405fdad43552689c22d0410589f3cd0a84743e6
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-28 11:01:23 +0300

    Users see only their borrowings

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import QuerySet
 from rest_framework import generics
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer
@@ -7,6 +8,12 @@ from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerialize
 class BorrowingListView(generics.ListCreateAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def get_queryset(self) -> QuerySet:
+        queryset = self.queryset
+        if not self.request.user.is_staff:
+            queryset = Borrowing.objects.filter(user_id=self.request.user.id)
+        return queryset
 
 
 class BorrowingDetailView(generics.RetrieveAPIView):
