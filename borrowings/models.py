64791d438895cee023695882e8890e976a4103e3
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-28 09:23:03 +0300

    Add borrowing model & make migrations

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from books.models import Book
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateTimeField(auto_now_add=True)
+    expected_return_date = models.DateTimeField()
+    actual_return_date = models.DateTimeField()
+    book_id = models.ForeignKey(to=Book, related_name="borrowings", on_delete=models.CASCADE)
+    user_id = models.ForeignKey(
+        to=get_user_model(), related_name="borrowings", on_delete=models.CASCADE
+    )
