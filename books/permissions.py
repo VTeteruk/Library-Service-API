eb7d571b74c63466bef0870836ca181b7c60dc4f
Vlad Teteruk <vladteteruk01@gmail.com>
2023-07-29 15:07:11 +0300

    Delete useless import & make IsAdminOrReadOnly permission shorter

--- books/permissions.py
+++ books/permissions.py
@@ -3,6 +3,4 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view) -> bool:
-        if request.method in SAFE_METHODS:
-            return True
-        return request.user.is_staff
+        return True if request.method in SAFE_METHODS else request.user.is_staff
